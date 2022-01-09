9c060b0628b24d712b460a137f972e6f4de39792
Matias Arias <matt.arias182@gmail.com>
2022-01-09 05:39:04 -0300

    Haciendo tareas de Objetos

--- app.js
+++ app.js
@@ -47,99 +47,252 @@ Listar todos los productos que estén en el carrito en orden alfabético
 
 // !------------------------ agregar productos al carrito -----------------------------
 
-let carrito = [];
+// let carrito = [];
 
-const agregarProducto = function () {
-    let agregar = prompt("Elegi el producto");
+// const agregarProducto = function () {
+//     let agregar = prompt("Elegi el producto");
 
-    while (agregar) {
-        carrito.push(agregar.toUpperCase());
+//     while (agregar) {
+//         carrito.push(agregar.toUpperCase());
 
-        agregar = prompt("Elegi el producto");
+//         agregar = prompt("Elegi el producto");
+//     }
+
+//     listarProductos(carrito)
+// };
+
+// // !---------------- eliminar un producto del carrito -----------------------
+
+// const eliminarProducto = function () {
+//     let producto = prompt("Ingrese el producto a eliminar");
+
+//     let indice = carrito.indexOf(producto.toUpperCase());
+
+//     if (indice >= 0) {
+//         let validar = confirm(
+//             `Está seguro que quiere eliminar ${carrito[indice]}`
+//         );
+
+//         if (validar) {
+//             carrito.splice(indice, 1);
+//             alert("Producto eliminado 🤙");
+//         }
+//     } else {
+//         alert("Producto no encontrado 😞");
+//     }
+// };
+
+// // !------------------------- listar productos --------------------------------
+
+// function listarProductos(arreglo) {
+
+//     if (arreglo.length > 0) {
+
+//         console.log('========= listado de productos =============');
+
+//         arreglo.map(function (item, index) {
+
+//             console.log(`${index + 1} - ${item}`);
+
+//         });
+
+//         console.log('================================');
+
+//     } else {
+
+//         console.log('no hay peliculas para listar');
+
+//     }
+
+// };
+
+// // ! ----------------------------- filtrar elementos del carrito -----------------------------
+
+// function filtrarProductos() {
+
+//     let termino = prompt('ingrese el producto a buscar')
+
+//     let nuevoCarrito = carrito.filter(function (articulo) {
+
+//         return articulo.includes(termino.toUpperCase());
+
+//     });
+
+//     if (nuevoCarrito.length > 0) {
+
+//         listarProductos(nuevoCarrito);
+
+//     } else {
+//         console.warn('no hay resultado para tu busqueda');
+//     }
+
+// };
+
+// // ! ----------------------- listar los productos en orden alfabetico ----------------------
+
+// function ordenDeLosProductos(articulo) {
+
+//      carrito.sort().map(function (item, index) {
+
+//         console.log(`${index + 1} - ${item}`)
+
+//     });
+
+// };
+
+// let valor1 = parseFloat(prompt("Ingresa tu primer valor"));
+// let valor2 = parseFloat(prompt("Ingresa tu segundo valor"));
+
+// function calculadora(num1, num2) {
+//     if (!num1 || !num2) {
+//         return "falta alguno de los numeros";
+//     }
+
+//     let operacion = prompt("Ingresa tu operación");
+
+//     switch (operacion) {
+//         case 'suma':
+//             return num1 + num2;
+
+//         case 'resta':
+//             return num1 - num2;
+
+//         case 'multi':
+//             return num1 * num2;
+
+//         case 'division':
+//             return num1 / num2;
+
+//         default:
+//             return alert("Operación invalida");
+//     }
+// }
+
+// const resultado = calculadora(valor1, valor2);
+
+// console.log('el resultado es',resultado);
+
+
+// function tablaDeMulti() {
+
+//     let multiplicar = parseInt(prompt('ingresa la tabla a calcular'))
+
+//     while (multiplicar) {
+//         console.log(`------------ Tabla del ${multiplicar} -----------------`);
+
+//         for (let index = 1; index <= 10; index++) {
+
+//             console.log(`${multiplicar} x ${index} ${multiplicar * index}`);
+
+//         }
+
+//         console.log('-------------------------------------');
+
+//         multiplicar = parseInt(prompt('ingresa la tabla a calcular'))
+
+
+
+//     }
+// }
+
+// tablaDeMulti()
+
+let arreglo = ['matias', 'juan pablo', 'viviana', 'carlos arias', 'evelyn', 'florencia']
+
+console.log(arreglo.indexOf('carlos arias'));
+
+// for (const persona of arreglo) {
+
+//     console.log(`Nosotros somos integrantes de la Familia Arias ${arreglo}`);
+
+// }
+
+const agregarFamiliar = function () {
+
+    let mascotas = prompt('ingresa nuevo integrante de la familia')
+
+
+    while (mascotas) {
+
+        arreglo.push(mascotas);
+
+        mascotas = prompt('ingresa nuevo integrante de la familia')
     }
 
-    listarProductos(carrito)
+}
+
+
+
+// *---------------------------------- Tarea de Objetos ------------------------------------
+
+/*
+?------------------------------ Objeto Persona ----------------------------------
+
+Crear un objeto llamado persona que contenga los siguientes atributos:
+Nombre
+Edad
+DNI
+Domicilio
+Hijos (cantidad)
+Profesión
+Agregar Métodos:
+Saludar ( mensaje de presentación en navegador o consola)
+Listar (muestra la lista con los datos de la persona)
+*/
+
+let persona = {
+    Nombre: 'Matias',
+    Edad: 33,
+    DNI: 34054750,
+    Domicilio: 'Brasil 470 Oeste',
+    Hijos: 'No',
+    Profesion: 'Front End Developer',
+
+    saludar() {
+        console.log(`Hola como están, Mi Nombre es: ${this.Nombre}`);
+    },
+
+    listar() {
+
+        for (const key in persona) {
+            console.log(`${key}: ${persona[key]}`);
+
+        };
+
+    },
 };
 
-// !---------------- eliminar un producto del carrito -----------------------
 
-const eliminarProducto = function () {
-    let producto = prompt("Ingrese el producto a eliminar");
 
-    let indice = carrito.indexOf(producto.toUpperCase());
+/*
+?---------------------------------------- Arreglo de objetos ------------------------------
 
-    if (indice >= 0) {
-        let validar = confirm(
-            `Está seguro que quiere eliminar ${carrito[indice]}`
-        );
+Crear un arreglo de objetos que contenga datos de películas como:
+Título
+Género
+Año
+sinopsis
+Crea una función que permita mostrar en una lista cada película con sus datos.
+*/
 
-        if (validar) {
-            carrito.splice(indice, 1);
-            alert("Producto eliminado 🤙");
-        }
-    } else {
-        alert("Producto no encontrado 😞");
-    }
-};
+let movies = [
+    { Titulo: 'Cinema Paradiso', Genero: 'Drama', Año: 1988, Sinopsis: 'Salvatore es un niño de un pueblo italiano, donde el único pasatiempo es ir al cine y Salvatore crece creyendo que el cine es magia.' },
 
-// !------------------------- listar productos --------------------------------
+    { Titulo: 'Merlí', Genero: 'Drama', Año: 2015, Sinopsis: 'Merlí sigue a un profesor de Filosofía muy peculirar. Merlí Bergeron vive con su madre tras ser desalojado y tiene además que aprender a convivir con su hijo Bruno. Cuando le contratan en el instituto Àngel Guimerà, el profesor invita a reflexionar y a opinar a sus alumnos en unas clases con métodos poco ortodoxos e imprevisibles. Más allá de los autores filosóficos Merlí utiliza la enseñanza para solucionar tanto sus problemas como los de sus alumnos.' },
 
-function listarProductos(arreglo) {
+    { Titulo: 'Gladiador', Genero: 'Drama', Año: 2000, Sinopsis: 'El general romano Máximo es el soporte más leal del emperador Marco Aurelio, que lo ha conducido de victoria en victoria. Sin embargo, Cómodo, el hijo de Marco Aurelio, está celoso del prestigio de Máximo y aún más del amor que su padre siente por él.' },
+];
 
-    if (arreglo.length > 0) {
 
-        console.log('========= listado de productos =============');
 
-        arreglo.map(function (item, index) {
+const mostrarPeliculas = function () {
 
-            console.log(`${index + 1} - ${item}`);
+    movies.map(function (movie) {
 
-        });
-
-        console.log('================================');
-
-    } else {
-
-        console.log('no hay peliculas para listar');
-
-    }
-
-};
-
-// ! ----------------------------- filtrar elementos del carrito -----------------------------
-
-function filtrarProductos() {
-
-    let termino = prompt('ingrese el producto a buscar')
-
-    let nuevoCarrito = carrito.filter(function (articulo) {
-
-        return articulo.includes(termino.toUpperCase());
+        console.log(movie);
 
     });
 
-    if (nuevoCarrito.length > 0) {
-
-        listarProductos(nuevoCarrito);
-
-    } else {
-        console.warn('no hay resultado para tu busqueda');
-    }
-
 };
-
-// ! ----------------------- listar los productos en orden alfabetico ----------------------
-
-function ordenDeLosProductos(articulo) {
-
-     carrito.sort().map(function (item, index) {
-
-        console.log(`${index + 1} - ${item}`)
-        
-    });
-    
-};
-
-
 
 
--- fundamentos/12-objetos.js
+++ fundamentos/12-objetos.js
@@ -1,36 +1,112 @@
 //-------objetos-----------------
 
 let persona = {
-    nombre: "Fabrizio",
-    apellido: "Tonin",
-    domicilio: "Gral Paz 576",
-    ciudad: "Tucumán",
-    saludar() {
-      console.log(`Hola, soy ${this.nombre} ${this.apellido}`);
-    },
-  };
-  
-  console.log(persona.nombre);
-  
-  //Cambiar el valor de una propiedad
-  persona.nombre = "Alfredo";
-  
-  persona.edad = 25;
-  
-  delete persona.saludar;
-  
-  for (const key in persona) {
-    console.log(`${key}: ${persona[key]}`);
-  }
-  
-  //arreglos de objetos
-  let cursos = [
-    { id: 1, titulo: "Javascript para principiantes", precio: 2500 },
-    { id: 2, titulo: "HTML introducción", precio: 1500 },
-    { id: 3, titulo: "CSS diseño responsive", precio: 2000 },
-    { id: 4, titulo: "Bootstrap 5 grillas responsive", precio: 2100 },
-  ];
-  
-  cursos.map(function (curso) {
-    console.log(curso.titulo);
+  nombre: "Fabrizio",
+  apellido: "Tonin",
+  domicilio: "Gral Paz 576",
+  ciudad: "Tucumán",
+  saludar() {
+    console.log(`Hola, soy ${this.nombre} ${this.apellido}`);
+  },
+};
+
+console.log(persona.nombre);
+
+//Cambiar el valor de una propiedad
+persona.nombre = "Alfredo";
+
+persona.edad = 25;
+
+delete persona.saludar;
+
+for (const key in persona) {
+  console.log(`${key}: ${persona[key]}`);
+}
+
+
+/*
+* Recordatorio: Para agregar o remplazar propiedades dentro de un objeto podemos usar por ej: persona.nombre, persona.apellido tanto para remplazar o crear nuevas propiedas, siempre poniendo el nombre del objeto seguido de un punto y el nombre de la propiedad a crear o remplazar, y otra forma es con corchetes, por ej: persona['nombre'] persona['ciudad], con corchetes sirve cuando iteramos con un for in. y en el console log usamos los corchetes. por ej: console.log(`${persona[key]}`). Key haria referencia a cada objeto dentro de un for in.
+*/
+
+//arreglos de objetos
+let cursos = [
+  { id: 1, titulo: "Javascript para principiantes", precio: 2500 },
+  { id: 2, titulo: "HTML introducción", precio: 1500 },
+  { id: 3, titulo: "CSS diseño responsive", precio: 2000 },
+  { id: 4, titulo: "Bootstrap 5 grillas responsive", precio: 2100 },
+];
+
+cursos.map(function (curso) {
+  console.log(curso.titulo);
+});
+
+
+// *---------------------------------- Tarea de Objetos ------------------------------------
+
+/*
+?------------------------------ Objeto Persona ----------------------------------
+
+Crear un objeto llamado persona que contenga los siguientes atributos:
+Nombre
+Edad
+DNI
+Domicilio
+Hijos (cantidad)
+Profesión
+Agregar Métodos:
+Saludar ( mensaje de presentación en navegador o consola)
+Listar (muestra la lista con los datos de la persona)
+*/
+
+let persona = {
+  Nombre: 'Matias',
+  Edad: 33,
+  DNI: 34054750,
+  Domicilio: 'Brasil 470 Oeste',
+  Hijos: 'No',
+  Profesion: 'Front End Developer',
+
+  saludar() {
+    console.log(`Hola como están, Mi Nombre es: ${this.Nombre}`);
+  },
+
+  listar() {
+
+    for (const key in persona) {
+      console.log(`${key}: ${persona[key]}`);
+
+    };
+  },
+};
+
+
+/*
+?---------------------------------------- Arreglo de objetos ------------------------------
+
+Crear un arreglo de objetos que contenga datos de películas como:
+Título
+Género
+Año
+sinopsis
+Crea una función que permita mostrar en una lista cada película con sus datos.
+*/
+
+let movies = [
+  { Titulo: 'Cinema Paradiso', Genero: 'Drama', Year: 1988, Sinopsis: 'Salvatore es un niño de un pueblo italiano, donde el único pasatiempo es ir al cine y Salvatore crece creyendo que el cine es magia.' },
+
+  { Titulo: 'Merlí', Genero: 'Drama', Year: 2015, Sinopsis: 'Merlí sigue a un profesor de Filosofía muy peculirar. Merlí Bergeron vive con su madre tras ser desalojado y tiene además que aprender a convivir con su hijo Bruno. Cuando le contratan en el instituto Àngel Guimerà, el profesor invita a reflexionar y a opinar a sus alumnos en unas clases con métodos poco ortodoxos e imprevisibles. Más allá de los autores filosóficos Merlí utiliza la enseñanza para solucionar tanto sus problemas como los de sus alumnos.' },
+
+  { Titulo: 'Gladiador', Genero: 'Drama', Year: 2000, Sinopsis: 'El general romano Máximo es el soporte más leal del emperador Marco Aurelio, que lo ha conducido de victoria en victoria. Sin embargo, Cómodo, el hijo de Marco Aurelio, está celoso del prestigio de Máximo y aún más del amor que su padre siente por él.' },
+];
+
+
+
+const mostrarPeliculas = function () {
+
+  movies.map(function (movie) {
+
+    console.log(movie);
+
   });
+
+};
